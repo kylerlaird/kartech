4466006d4cfd4597e040c5ac3c73254456feea34
Volodymyr Tarasenko <tvntsr@yahoo.com>
2017-08-07 02:54:39 +0300

    Version test command is added

---
+++ tests/version.py
@@ -0,0 +1,29 @@
+import actuator
+import binascii
+import threading
+
+
+def OnFrameSending(frame):
+    print("-> [%s]: %s" % (hex(frame.frame_id), binascii.hexlify(frame.frame_data).decode()))
+
+
+def OnFrameReceiving(frame):
+    print("<- [%s]: %s" % (hex(frame.frame_id), binascii.hexlify(frame.frame_data).decode()))
+
+
+act = actuator.Actuator("vcan0")
+act.Ignore(0x00FF0000)
+try:
+    act.StartSendingBus(3, OnFrameSending)
+    act.StartReceivingBus(3, OnFrameReceiving)
+    msg = act.SoftwareVersionData(False, True)
+    if msg is None:
+        print ("Wrong response received")
+    print ("Version %s" % msg.SoftwareVersion())
+    print ("Date (DD/MM/YYYY): %s/%s/%s" % (msg.SwDay(), msg.SwMonth(), msg.SwYear()))
+
+finally:
+    act.StopSendingBus()
+    act.StopSendingBus()
+
+print("Execution DONE.")
